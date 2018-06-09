49b707356d6d410291c41f972840cb5de189f6e1
John Knoop <john.r.knoop@gmail.com>
2018-06-09 16:47:53 -0400

    Added content to project and about me pages

--- Scripts/app.js
+++ Scripts/app.js
@@ -26,7 +26,31 @@
 
 
     function AboutContent() {
+        //Header for About page
+       let aboutTitle= document.getElementById("aboutTitle");
+       aboutTitle.textContent="About Me";
 
+        //Mission Statement
+        let missionStatement=document.getElementById("missionStatement");
+        missionStatement.textContent="My mission is to work towards my diploma and hopefully enjoy doing it. Afterwards I hope to enjoy games, specifically online MMO's.";;
+    }
+
+    function ProjectsContent() {
+        let titleProjects= document.getElementById ("titleProjects");
+        titleProjects.textContent="Past Projects";
+
+        let projectTitle1=document.getElementById("projectTitle1");
+        projectTitle1.textContent="Careers Game of Life";
+
+        let project1Content= document.getElementById("project1Content");
+        project1Content.textContent="This is a project that I did in 10th grade for Careers class. We were required to create our own version of the game of life. I unfortunately do not have any photos of the full game but do have photos of these game pieces I made!";
+
+        let projectTitle2=document.getElementById("projectTitle2");
+        projectTitle2.textContent="TEST";
+
+        let project2Content= document.getElementById("project2Content");
+        project2Content.textContent="This is placeholder text";
+        
     }
 
     function Start() {
@@ -44,6 +68,10 @@
 
             case "COMP125 - About":
                 AboutContent();
+                break;
+
+            case "COMP125 - Projects":
+                ProjectsContent();
                 break;
 
             default:
