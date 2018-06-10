31bb744042e067d58da015488e3d276dc39fe940
John Knoop <john.r.knoop@gmail.com>
2018-06-10 14:02:50 -0400

    Added another project. Added a list to about.

--- Scripts/app.js
+++ Scripts/app.js
@@ -1,26 +1,9 @@
-/* Custom JS goes here */
-
-// IIFE - Immediately Invoked Function Expression
-// anonymous self executing function
+// Custom JS
 (function () {
     "use strict"
 
-    // About Button Click event handler
-    function AboutButtonClick() {
-        console.log("About Button Clicked!");
-    }
-
-    function AboutButtonOver(event) {
-        event.currentTarget.style.opacity = 0.3;
-    }
-
-    function AboutButtonOut(event) {
-        event.currentTarget.style.opacity = 1.0;
-    }
 
     function HomeContent() {
-
-
 
     }
 
@@ -33,11 +16,24 @@
         //Mission Statement
         let missionStatement=document.getElementById("missionStatement");
         missionStatement.textContent="My mission is to work towards my diploma and hopefully enjoy doing it. Afterwards I hope to enjoy games, specifically online MMO's.";;
+        //list of favorites
+        let favorites = [
+            "Video Games",
+            "Music",
+            "Film",
+            "Science"
+        ];
+        let myFavorites = document.getElementById("favorites");
+        favorites.forEach(item => {
+            let newItem = document.createElement("li");
+            newItem.textContent = item;
+            myFavorites.appendChild(newItem);
+        });
     }
 
     function ProjectsContent() {
         let titleProjects= document.getElementById ("titleProjects");
-        titleProjects.textContent="Past Projects";
+        titleProjects.textContent="Projects";
 
         let projectTitle1=document.getElementById("projectTitle1");
         projectTitle1.textContent="Careers Game of Life";
@@ -46,10 +42,10 @@
         project1Content.textContent="This is a project that I did in 10th grade for Careers class. We were required to create our own version of the game of life. I unfortunately do not have any photos of the full game but do have photos of these game pieces I made!";
 
         let projectTitle2=document.getElementById("projectTitle2");
-        projectTitle2.textContent="TEST";
+        projectTitle2.textContent="Friends of Star Wars Galaxies";
 
         let project2Content= document.getElementById("project2Content");
-        project2Content.textContent="This is placeholder text";
+        project2Content.textContent="7 years ago the MMO Star Wars Galaxies shutdown. However a new version was released 2 years ago coded by fans.  Currently a project I am working on in my free time is helping to get all of my old friends I made in this game almost a decade ago ready to particpate in endgame team based challenges once again.";
         
     }
 
