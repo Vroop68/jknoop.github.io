abea6d24b67eba13c3eeb9e4eb1e9c89818c70d3
John Knoop <john.r.knoop@gmail.com>
2018-06-13 11:33:02 -0400

    Added comments, removed un needed CSS

--- Scripts/app.js
+++ Scripts/app.js
@@ -37,28 +37,29 @@
     }
 
     function ProjectsContent() {
+        //title for projects page
         let titleProjects= document.getElementById ("titleProjects");
         titleProjects.textContent="Projects";
-
+        //custom JS for project1
         let projectTitle1=document.getElementById("projectTitle1");
         projectTitle1.textContent="Careers Game of Life";
 
         let project1Content= document.getElementById("project1Content");
         project1Content.textContent="This is a project that I did in 10th grade for Careers class. We were required to create our own version of the game of life. I unfortunately do not have any photos of the full game but do have photos of these game pieces I made!";
-
+        //JS for project2
         let projectTitle2=document.getElementById("projectTitle2");
         projectTitle2.textContent="Friends of Star Wars Galaxies";
 
         let project2Content= document.getElementById("project2Content");
         project2Content.textContent="7 years ago the MMO Star Wars Galaxies shutdown. However a new version was released 2 years ago coded by fans.  Currently a project I am working on in my free time is helping to get all of my old friends I made in this game almost a decade ago ready to particpate in endgame team based challenges once again.";
-        
+        //JS for project3
         let projectTitle3=document.getElementById("projectTitle3");
         projectTitle3.textContent="Working With Udemy";
 
         let project3Content= document.getElementById("project3Content");
         project3Content.textContent="Over the years I have used Udemy as a learning tool. While lately school has kept be too busy to work on it I have used Udemy to learn about Java, HTML and CSS, some C# and C++ as well as some other subjects.  I have found it to be a great learning tool!";
     }
-
+    //JS for contact page
     function ContactContent(){
         let submitBtn = document.getElementById("submitBtn")
         submitBtn.addEventListener("click", function(event){
@@ -67,9 +68,7 @@
         });
     }
 
-    function Start() {
-        // local variable
-        let title = document.title;
+    function Start() {        let title = document.title;
 
         console.log("App Started!");
         console.log("----------------------------");
